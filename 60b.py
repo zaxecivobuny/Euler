aec5c63c15eba949ddb33c5a847d5c6c052a9399
zaxecivobuny <zaxecivobuny@gmail.com>
2015-04-25 20:37:54 -0700

    new version of 60, stuck on permutations

---
+++ 60b.py
@@ -0,0 +1,18 @@
+from useful import checkprime, get_primes
+
+# start with a set of n primes, where n is 4 in the problem example
+# check all iterations of that prime set in ascending sum order
+# if found, return the set
+# otherwise, increase the set size by one prime and repeat this and the last two steps
+
+
+# start by setting listlength to n and getting an initial set of 10,000 primes to draw on for later
+
+listlength = 4
+master_list = []
+for i in get_primes(10000):
+    master_list.append(i)
+
+# seedlist will be the subset of the master list that our workinglist draws from. It's max will increment in every greater cycle    
+seedlist = master_list[:listlength]
+
